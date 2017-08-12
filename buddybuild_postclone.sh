0cf1065ec2515e871b2379041ee7d60d49e3d42b
David Lam <david@buddybuild.com>
2017-08-11 18:17:16 -0700

    test

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "export APP_VERSION=asdf" >> $HOME/.bash_profile
+echo "export APP_VERSION=1" >> $HOME/.bash_profile
--- buddybuild_prebuild.sh
+++ buddybuild_prebuild.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 echo $APP_VERSION
+echo $BUDDYBUILD_WORKSPACE
